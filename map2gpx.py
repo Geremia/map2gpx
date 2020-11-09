e4394e4cdeaa12d90b69d8eeb051a4c3fe33d62d
Geremia <Geremia@users.noreply.github.com>
2020-11-09 13:54:33 -0700

    parses KPH column now and adds corresponding GPX extension tag for KPH ☧ gratias.

--- map2gpx.py
+++ map2gpx.py
@@ -8,21 +8,25 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 map_file_name = sys.argv[1]
+map_file_name_basename = map_file_name[:-4]
+fout = open(map_file_name_basename + '.gpx', 'w')
 
 print("""<?xml version="1.0" encoding="UTF-8" ?>
 <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="map2gpx - https://github.com/Geremia/map2gpx" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd ">
 	<trk>
 		<name><![CDATA[""" + map_file_name + """]]> </name>
-		<trkseg>""")
+		<trkseg>""", file=fout)
 
 with open(map_file_name, newline='') as csvfile:
     data = list(csv.reader(csvfile))
 
 #['A', '061120', '092833', '3322.2928', 'N', '11204.7423', 'W', '000.00', '0.00', '0.00', '0.00;']
 
+
 for i in data:
     date_str = i[1]
     time = i[2]
+    kph = str(round(float(i[7]),6))
 
     latsign = lonsign = ''
     if i[4] == 'S':
@@ -39,10 +43,15 @@ for i in data:
     second = time[4:6]
 
     if i[0] == 'A':
-        print("			<trkpt lat=\"" + lat + "\" lon=\"" + lon + "\">")
-        print("				<time>" + year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':' + second + 'Z</time>')
-        print("			</trkpt>")
+        print("			<trkpt lat=\"" + lat + "\" lon=\"" + lon + """">
+				<time>""" + year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':' + second + """Z</time>
+				<extensions>
+					<speed>""" + kph + """</speed>
+				</extensions>
+			</trkpt>""", file=fout)
 
 print("""		</trkseg>
 	</trk>
-</gpx>""")
+</gpx>""", file=fout)
+
+fout.close()
